9721691850f1ce72ad68904a1d3f346858153a8f
Tanner Lake <tanner.lake@gmail.com>
2019-11-15 20:50:26 -0800

    add debug flag, set default parallel=2

--- cmd/ecs_task.go
+++ cmd/ecs_task.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/quintilesims/go-ecs-cleaner/ecstask"
 	"github.com/spf13/cobra"
 )
@@ -13,9 +16,15 @@ var ecsTaskCmd = &cobra.Command{
 BEFORE RUNNING: Make sure that you've properly configured your environment with
 the AWS CLI for the AWS account you want to clean up.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if parallelFlag < 1 {
+			fmt.Println("minimum parallel is 1")
+			os.Exit(1)
+		}
+
 		flags := map[string]interface{}{
 			"apply":    applyFlag,
 			"cutoff":   cutoffFlag,
+			"debug":    debugFlag,
 			"parallel": parallelFlag,
 			"region":   regionFlag,
 			"verbose":  verboseFlag,
@@ -27,6 +36,7 @@ the AWS CLI for the AWS account you want to clean up.`,
 
 var applyFlag bool
 var cutoffFlag int
+var debugFlag bool
 var parallelFlag int
 var regionFlag string
 var verboseFlag bool
@@ -34,7 +44,8 @@ var verboseFlag bool
 func init() {
 	ecsTaskCmd.Flags().BoolVarP(&applyFlag, "apply", "a", false, "actually perform task definition deregistration")
 	ecsTaskCmd.Flags().IntVarP(&cutoffFlag, "cutoff", "c", 5, "how many most-recent task definitions to keep around")
-	ecsTaskCmd.Flags().IntVarP(&parallelFlag, "parallel", "p", 10, "how many concurrent deregistration requests to make")
+	ecsTaskCmd.Flags().BoolVarP(&debugFlag, "debug", "d", false, "enable for all the output")
+	ecsTaskCmd.Flags().IntVarP(&parallelFlag, "parallel", "p", 2, "how many concurrent deregistration requests to make")
 	ecsTaskCmd.Flags().StringVarP(&regionFlag, "region", "r", "us-west-2", "the AWS region in which to operate")
 	ecsTaskCmd.Flags().BoolVarP(&verboseFlag, "verbose", "v", false, "enable for chattier output")
 	rootCmd.AddCommand(ecsTaskCmd)
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/aws/aws-sdk-go v1.25.34
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
+	github.com/jpillora/backoff v1.0.0
 	github.com/spf13/cobra v0.0.5
 	golang.org/x/net v0.0.0-20191112182307-2180aed22343 // indirect
 )
