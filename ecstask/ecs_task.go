d8c614a033980e109b4fca593031b184e0b951f5
Tanner Lake <tanner.lake@gmail.com>
2019-11-15 20:53:47 -0800

    implement exponential backoff across goroutines for task definition deregistration

--- ecstask/ecs_task.go
+++ ecstask/ecs_task.go
@@ -2,13 +2,18 @@ package ecstask
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/golang-collections/collections/stack"
+	"github.com/jpillora/backoff"
 	"github.com/spf13/cobra"
 )
 
@@ -269,9 +274,9 @@ func Run(cmd *cobra.Command, args []string, flags map[string]interface{}) {
 		fmt.Println("\n`--apply` flag present")
 		fmt.Printf("deregistering %d task definitions...\n", len(allTaskDefinitionArns))
 
-		deregisterTaskDefinitions(svc, allTaskDefinitionArns, flags["parallel"].(int))
+		deregisterTaskDefinitions(svc, allTaskDefinitionArns, flags["parallel"].(int), flags["debug"].(bool))
 
-		fmt.Println("finished")
+		fmt.Println("\nfinished")
 	} else {
 		fmt.Println("\nthis is a dry run")
 		fmt.Println("use the `--apply` flag to deregister these task definitions")
@@ -397,44 +402,177 @@ func removeAFromB(a, b []string) []string {
 	return diff
 }
 
-func deregisterTaskDefinitions(svc *ecs.ECS, taskDefinitionArns []string, parallel int) {
-	arnsChan := make(chan string, len(taskDefinitionArns))
+// Job carries information through a Job channel.
+type Job struct {
+	Arn string
+}
 
-	deregisterTaskDefinition := func(arn string) {
-		_, err := svc.DeregisterTaskDefinition(&ecs.DeregisterTaskDefinitionInput{
-			TaskDefinition: aws.String(arn),
-		})
-		if err != nil {
-			fmt.Println("Error deregistering task definition:", err)
-		}
-	}
+// Result carries informatino through a Result chan.
+type Result struct {
+	Arn string
+	Err error
+}
 
-	worker := func(wg *sync.WaitGroup) {
-		for arn := range arnsChan {
-			deregisterTaskDefinition(arn)
+func deregisterTaskDefinitions(svc *ecs.ECS, taskDefinitionArns []string, parallel int, debug bool) {
+	worker := func(wg *sync.WaitGroup, jobsChan <-chan Job, resultsChan chan<- Result) {
+		for job := range jobsChan {
+			input := &ecs.DeregisterTaskDefinitionInput{
+				TaskDefinition: aws.String(job.Arn),
+			}
+
+			_, err := svc.DeregisterTaskDefinition(input)
+
+			result := Result{Arn: job.Arn, Err: err}
+			resultsChan <- result
 		}
 
 		wg.Done()
 	}
 
-	createWorkerPool := func(numWorkers int) {
-		var wg sync.WaitGroup
-		for i := 0; i < numWorkers; i++ {
-			wg.Add(1)
-			go worker(&wg)
-		}
-
-		wg.Wait()
-	}
-
-	allocate := func(arns []string) {
+	dispatcher := func(wg *sync.WaitGroup, arns []string, parallel int, jobsChan chan Job, resultsChan chan Result) {
+		jobs := stack.New()
 		for _, arn := range arns {
-			arnsChan <- arn
+			jobs.Push(Job{arn})
 		}
 
-		close(arnsChan)
+		var completedJobs int
+
+		preload := 1
+		if parallel > 1 {
+			preload = parallel - 1
+		}
+
+		for i := 0; i < preload; i++ {
+			jobsChan <- jobs.Pop().(Job)
+		}
+
+		b := &backoff.Backoff{
+			Min:    100 * time.Millisecond,
+			Max:    2 * time.Minute,
+			Jitter: true,
+		}
+
+		for result := range resultsChan {
+			if result.Err != nil {
+				if !isThrottlingError(result.Err) {
+					fmt.Println("\nError deregistering task definition,", result.Err)
+					close(jobsChan)
+					close(resultsChan)
+					wg.Done()
+					os.Exit(1)
+				}
+
+				t := b.Duration()
+				if debug {
+					fmt.Printf("\nbackoff triggered for %s,", result.Arn)
+					fmt.Printf("\nwaiting for %v\n", t)
+				}
+
+				time.Sleep(t)
+				jobs.Push(Job{Arn: result.Arn})
+			} else {
+				b.Reset()
+				completedJobs++
+				fmt.Printf("\r%d deregistered task definitions", completedJobs)
+			}
+
+			if jobs.Len() > 0 {
+				jobsChan <- jobs.Pop().(Job)
+			}
+
+			if completedJobs == len(arns) {
+				close(jobsChan)
+				close(resultsChan)
+			}
+		}
+
+		wg.Done()
 	}
 
-	go allocate(taskDefinitionArns)
-	createWorkerPool(parallel)
+	jobsChan := make(chan Job, parallel)
+	resultsChan := make(chan Result, parallel)
+
+	var wg sync.WaitGroup
+	for i := 0; i < parallel; i++ {
+		wg.Add(1)
+		go worker(&wg, jobsChan, resultsChan)
+	}
+
+	wg.Add(1)
+	go dispatcher(&wg, taskDefinitionArns, parallel, jobsChan, resultsChan)
+
+	wg.Wait()
+
+	// worker := func(wg *sync.WaitGroup) {
+	// 	for arn := range arnsChan {
+	// 		deregisterTaskDefinition(arn, throttledChan, countChan)
+	// 	}
+
+	// 	wg.Done()
+	// }
+
+	// progressTracker := func(countChan <-chan int) {
+	// 	var counter int
+	// 	fmt.Printf("\r%d task definitions deregistered", counter)
+
+	// 	for i := range countChan {
+	// 		counter += i
+	// 		//fmt.Printf("\r%d task definitions deregistered", counter)
+	// 	}
+	// }
+
+	// dispatcher := func(arns []string, arnsChan chan<- string, throttledChan <-chan bool) {
+	// 	b := &backoff.Backoff{
+	// 		Min:    100 * time.Millisecond,
+	// 		Max:    5 * time.Minute,
+	// 		Jitter: true,
+	// 	}
+
+	// 	for _, arn := range arns {
+	// 		for throttled := range throttledChan {
+	// 			fmt.Printf("%t\n", throttled)
+	// 			if throttled {
+	// 				fmt.Printf("waiting for %v\n", b.Duration())
+	// 			} else {
+	// 				fmt.Printf("resetting\n")
+	// 				b.Reset()
+	// 			}
+	// 		}
+	// 	}
+
+	// }
+
+	// arnsChan := make(chan string, len(taskDefinitionArns))
+	// countChan := make(chan int, parallel)
+	// throttledChan := make(chan bool, parallel)
+
+	// go dispatcher(taskDefinitionArns, arnsChan, throttledChan)
+	// go progressTracker(countChan)
+
+	// var wg sync.WaitGroup
+	// for i := 0; i < parallel; i++ {
+	// 	wg.Add(1)
+	// 	go worker(&wg)
+	// }
+
+	// wg.Wait()
+	// close(countChan)
+	// close(throttledChan)
+}
+
+func isThrottlingError(err error) bool {
+	if awsErr, ok := err.(awserr.Error); ok {
+		code := awsErr.Code()
+
+		if code == "Throttling" || code == "ThrottlingException" {
+			return true
+		}
+
+		message := strings.ToLower(awsErr.Message())
+		if code == "ClientException" && strings.Contains(message, "too many concurrent attempts") {
+			return true
+		}
+	}
+
+	return false
 }
