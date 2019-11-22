293590ac1b2d27c8c63200d8d75b876c7061b59f
Tanner Lake <tanner.lake@gmail.com>
2019-11-22 14:08:12 -0800

    remove unneccessary iface

--- ecsclient/interface.go
+++ ecsclient/interface.go
@@ -2,19 +2,6 @@ package ecsclient
 
 import "github.com/aws/aws-sdk-go/service/ecs"
 
-// Iface defines the interface required for an object to satisfy in order to perform
-// as the ECSClient struct.
-type Iface interface {
-	CleanupTaskDefinitions() error
-	CollectClusters() ([]string, error)
-	CollectServices([]string) (map[string][]string, error)
-	CollectTaskDefinitions() ([]string, error)
-	ConfigureSession() error
-	DeregisterTaskDefinitions([]string) error
-	DescribeServices(map[string][]string) ([]ecs.Service, error)
-	FilterTaskDefinitions([]string, []ecs.Service) ([]string, error)
-}
-
 // ECSSvc defines the methods that an object must have in order to be used as the `Svc`
 // object in the ECSClient. The AWS `ecs.ECS` object satisfies this interface.
 type ECSSvc interface {
