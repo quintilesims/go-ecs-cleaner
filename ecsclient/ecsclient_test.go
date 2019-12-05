a060b0daa6b373f1e52fad2c0aa647de6c68c158
Tanner Lake <tanner.lake@gmail.com>
2019-12-05 14:04:03 -0800

    test collect* methods, add skeleton for more tests

--- ecsclient/ecsclient_test.go
+++ ecsclient/ecsclient_test.go
@@ -2,6 +2,7 @@ package ecsclient
 
 import (
 	"errors"
+	"reflect"
 	"testing"
 	"time"
 
@@ -13,15 +14,23 @@ import (
 	"github.com/quintilesims/go-ecs-cleaner/mocks"
 )
 
-func TestCollectClusters(t *testing.T) {
+func setupTest(t *testing.T) (*gomock.Controller, *ECSClient, *mocks.MockECSAPI) {
 	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
 
 	e := NewECSClient()
 	e.Flags.Quiet = true
 
 	svc := mocks.NewMockECSAPI(ctrl)
 	e.Svc = svc
+
+	return ctrl, e, svc
+}
+
+func Test_CollectClusters(t *testing.T) {
+	ctrl, e, svc := setupTest(t)
+	defer ctrl.Finish()
+
+	expected := []string{"arn0", "arn1", "arn2"}
 
 	svc.EXPECT().
 		ListClusters(&ecs.ListClustersInput{
@@ -41,29 +50,108 @@ func TestCollectClusters(t *testing.T) {
 			NextToken:   nil,
 		}, nil)
 
-	expectedClusters := []string{"arn0", "arn1", "arn2"}
-	clusters, err := e.CollectClusters()
-
+	result, err := e.CollectClusters()
 	if err != nil {
-		t.Error("error encountered, ", err)
+		t.Error(err)
 	}
 
-	for i := 0; i < len(expectedClusters); i++ {
-		if clusters[i] != expectedClusters[i] {
-			t.Errorf("clusters[%d] (%s) != expectedClusters[%d] (%s)", i, clusters[i], i, expectedClusters[i])
-		}
+	if equal := reflect.DeepEqual(expected, result); !equal {
+		t.Errorf("Expected %v, got %v\n", expected, result)
 	}
 }
 
-func TestDeregisterTaskDefinitions_SunnyDay(t *testing.T) {
-	ctrl := gomock.NewController(t)
+func Test_CollectServices(t *testing.T) {
+	ctrl, e, svc := setupTest(t)
 	defer ctrl.Finish()
 
-	e := NewECSClient()
-	e.Flags.Quiet = true
+	clusterARNs := []string{"cluster1", "cluster0"}
 
-	svc := mocks.NewMockECSAPI(ctrl)
-	e.Svc = svc
+	expected := map[string][]string{
+		"cluster0": []string{"service0", "service1"},
+		"cluster1": []string{"service2"},
+	}
+
+	// paginated result
+	svc.EXPECT().
+		ListServices(&ecs.ListServicesInput{
+			Cluster:   aws.String("cluster0"),
+			NextToken: nil,
+		}).
+		Return(&ecs.ListServicesOutput{
+			ServiceArns: []*string{aws.String("service0")},
+			NextToken:   aws.String("a"),
+		}, nil)
+
+	svc.EXPECT().
+		ListServices(&ecs.ListServicesInput{
+			Cluster:   aws.String("cluster0"),
+			NextToken: aws.String("a"),
+		}).
+		Return(&ecs.ListServicesOutput{
+			ServiceArns: []*string{aws.String("service1")},
+			NextToken:   nil,
+		}, nil)
+
+	// unpaginated result
+	svc.EXPECT().
+		ListServices(&ecs.ListServicesInput{
+			Cluster:   aws.String("cluster1"),
+			NextToken: nil,
+		}).
+		Return(&ecs.ListServicesOutput{
+			ServiceArns: []*string{aws.String("service2")},
+			NextToken:   nil,
+		}, nil)
+
+	result, err := e.CollectServices(clusterARNs)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if equal := reflect.DeepEqual(expected, result); !equal {
+		t.Errorf("Expected %v, got %v\n", expected, result)
+	}
+}
+
+func Test_CollectTaskDefinitions(t *testing.T) {
+	ctrl, e, svc := setupTest(t)
+	defer ctrl.Finish()
+
+	expected := []string{"arn0", "arn1", "arn2"}
+
+	svc.EXPECT().
+		ListTaskDefinitions(&ecs.ListTaskDefinitionsInput{}).
+		Return(&ecs.ListTaskDefinitionsOutput{
+			TaskDefinitionArns: []*string{aws.String("arn0"), aws.String("arn1")},
+			NextToken:          aws.String("a"),
+		}, nil)
+
+	svc.EXPECT().
+		ListTaskDefinitions(&ecs.ListTaskDefinitionsInput{
+			NextToken: aws.String("a"),
+		}).
+		Return(&ecs.ListTaskDefinitionsOutput{
+			TaskDefinitionArns: []*string{aws.String("arn2")},
+			NextToken:          nil,
+		}, nil)
+
+	result, err := e.CollectTaskDefinitions()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if equal := reflect.DeepEqual(expected, result); !equal {
+		t.Errorf("Expected %v, got %v\n", expected, result)
+	}
+}
+
+func Test_DescribeServices(t *testing.T) {
+
+}
+
+func Test_DeregisterTaskDefinitions_SunnyDay(t *testing.T) {
+	ctrl, e, svc := setupTest(t)
+	defer ctrl.Finish()
 
 	arns := []string{"arn0", "arn1", "arn2"}
 
@@ -78,15 +166,9 @@ func TestDeregisterTaskDefinitions_SunnyDay(t *testing.T) {
 	}
 }
 
-func TestDeregisterTaskDefinitions_StopworthyError(t *testing.T) {
-	ctrl := gomock.NewController(t)
+func Test_DeregisterTaskDefinitions_StopworthyError(t *testing.T) {
+	ctrl, e, svc := setupTest(t)
 	defer ctrl.Finish()
-
-	e := NewECSClient()
-	e.Flags.Quiet = true
-
-	svc := mocks.NewMockECSAPI(ctrl)
-	e.Svc = svc
 
 	arns := []string{"arn0"}
 
@@ -107,12 +189,9 @@ func TestDeregisterTaskDefinitions_StopworthyError(t *testing.T) {
 	}
 }
 
-func TestDeregisterTaskDefinitions_ThrottlingError(t *testing.T) {
-	ctrl := gomock.NewController(t)
+func Test_DeregisterTaskDefinitions_ThrottlingError(t *testing.T) {
+	ctrl, e, svc := setupTest(t)
 	defer ctrl.Finish()
-
-	e := NewECSClient()
-	e.Flags.Quiet = true
 
 	b := backoff.Backoff{
 		Min:    1 * time.Millisecond,
@@ -121,9 +200,6 @@ func TestDeregisterTaskDefinitions_ThrottlingError(t *testing.T) {
 	}
 
 	e.Backoff = &b
-
-	svc := mocks.NewMockECSAPI(ctrl)
-	e.Svc = svc
 
 	arns := []string{"arn0"}
 
@@ -169,4 +245,36 @@ func TestDeregisterTaskDefinitions_ThrottlingError(t *testing.T) {
 	if err != nil {
 		t.Error("error encountered: ", err)
 	}
+}
+
+func Test_FilterTaskDefinitions(t *testing.T) {
+
+}
+
+func Test_listClusters(t *testing.T) {
+
+}
+
+func Test_listServices(t *testing.T) {
+
+}
+
+func Test_listTaskDefinitions(t *testing.T) {
+
+}
+
+func Test_describeServices(t *testing.T) {
+
+}
+
+func Test_isExpiredTokenError(t *testing.T) {
+
+}
+
+func Test_isStopworthyError(t *testing.T) {
+
+}
+
+func Test_isThrottlingError(t *testing.T) {
+
 }
